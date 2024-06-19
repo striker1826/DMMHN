39b8154371b5d9762c86beb647031a7e8b10e280
striker1826 <alstjq123579@daum.net>
2024-06-19 11:03:03 +0900

    feat: token으로 user 정보 찾아오는 API Swagger 추가

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,11 +2,18 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from 'src/common/decorators/user-decorator';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
+@ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({
+    description: '카카오에서 받은 인증 코드로 accessToken을 받을 수 있는 API 입니다.',
+  })
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse()
   @UseGuards(AuthGuard('jwt'))
   @Get('token')
   async getUsersByToken(@User() userId: number) {
