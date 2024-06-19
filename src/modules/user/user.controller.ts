a3ca193f1c04ec4383dcf4e6165d8058ee4286af
striker1826 <alstjq123579@daum.net>
2024-06-20 08:57:22 +0900

    feat: user/info API에 Swagger용 BearerAuth 추가

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,7 +2,14 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from 'src/common/decorators/user-decorator';
-import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @ApiTags('User')
 @Controller('user')
@@ -12,6 +19,7 @@ export class UserController {
   @ApiOperation({
     description: '유저의 access_token으로 유저의 정보를 가져오는 API 입니다.',
   })
+  @ApiBearerAuth('access-token')
   @ApiOkResponse()
   @ApiUnauthorizedResponse()
   @UseGuards(AuthGuard('jwt'))
