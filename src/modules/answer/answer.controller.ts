b5e9466fbef80b5d34053c333d51e796eac87383
striker1826 <alstjq123579@daum.net>
2024-09-16 16:30:56 +0900

    feat: GPT API 면접 질문, 대답 처리 API Swagger 추가

--- src/modules/answer/answer.controller.ts
+++ src/modules/answer/answer.controller.ts
@@ -3,12 +3,15 @@ import { AnswerService } from './answer.service';
 import { GptService } from '../gpt/gpt.service';
 import { GradingDto } from './dto/gradingDto';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Answer')
 @Controller('answer')
 export class AnswerController {
   constructor(private readonly answerService: AnswerService, private readonly gptService: GptService) {}
 
   // @UseGuards(AuthGuard('jwt'))
+  @ApiOperation({ description: 'GPT API을 이용해서 면접 질문과 대답 처리하기' })
   @Post('grading')
   async gradingAnswer(@Body() body: { stacks: string[]; question: string; answer: string }) {
     const result = await this.answerService.getMessagesData(body);
