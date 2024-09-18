1aea73298189c34a728d2a20499a9c3b0413a32d
striker1826 <alstjq123579@daum.net>
2024-09-18 21:27:51 +0900

    fix: cookie 도메인 변경

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -34,14 +34,14 @@ export class AuthController {
       sameSite: 'none',
       secure: true,
       maxAge: 1000 * 60 * 60 * 24,
-      domain: 'localhost',
+      domain: 'http://localhost:3000',
     });
     res.cookie('refreshToken', result.refresh_token, {
       httpOnly: true,
       sameSite: 'none',
       secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 7,
-      domain: 'localhost',
+      domain: 'http://localhost:3000',
     });
     return res.json(result).status(200);
   }
