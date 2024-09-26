373e2bcd433948d4347b9054f611516f2c259ff7
striker1826 <alstjq123579@daum.net>
2024-09-26 14:36:43 +0900

    Feat: 이전에 했던 질문내용 기록하기

--- src/modules/answer/answer.service.ts
+++ src/modules/answer/answer.service.ts
@@ -7,7 +7,7 @@ import { GradingDto } from './dto/gradingDto';
 export class AnswerService {
   constructor(private readonly gptService: GptService) {}
 
-  async getMessagesData(body: { stacks: string[]; question: string; answer: string }) {
+  async getMessagesData(body: { stacks: string[]; question: string; answer: string; previousQuestions: string[] }) {
     const stacksString = body.stacks.join(', ');
     // const promptStringTypeList = answerDto.map(
     //   (item) => `질문: ${item.question}. 답변: ${item.answer ? item.answer : '몰라요'}`,
@@ -27,6 +27,7 @@ export class AnswerService {
           content:
             `keywords: ${stacksString}` +
             process.env.OPENAI_SYSTEM_TEXT +
+            `이전에 했던 질문 목록: ${body.previousQuestions.join(', ')}` +
             `$$${body.question}$$` +
             `%%${body.answer ? body.answer : '몰라요'}%%`,
         },
--- src/modules/answer/dto/gradingDto.ts
+++ src/modules/answer/dto/gradingDto.ts
@@ -9,4 +9,7 @@ export class GradingDto {
 
   @ApiProperty()
   stacks: string[];
+
+  @ApiProperty()
+  previousQuestions: string[];
 }
