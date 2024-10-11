4f122b82c2ffe387abd33c9d19533ffeaa02b7fa
striker1826 <alstjq123579@daum.net>
2024-10-12 08:41:58 +0900

    fix: /grading/evaluation API 경로에서 answer 데이터를 잘못 보냈을 경우 에러메세지가 잘못 리턴되는 버그 해결
    - question은 문자열이어야 합니다. --> answer은 문자열이어야 합니다.
    - question은 필수값입니다. --> answer은 필수값입니다.

--- src/modules/grading/dto/input/evaluation.dto.ts
+++ src/modules/grading/dto/input/evaluation.dto.ts
@@ -13,7 +13,7 @@ export class EvaluationInputDto {
     example:
       '호이스팅이란 자바스크립트가 메모리를 미리 확보하는 작업을 의미합니다. 해당 작업은 마치 식별자들을 위로 끌어올리는 것 같은 착각을 주기 때문에 호이스팅이라는 이름이 붙게 되었습니다.',
   })
-  @IsString({ message: 'question은 문자열이어야 합니다.' })
-  @IsNotEmpty({ message: 'question은 필수값입니다.' })
+  @IsString({ message: 'answer은 문자열이어야 합니다.' })
+  @IsNotEmpty({ message: 'answer은 필수값입니다.' })
   answer: string;
 }
